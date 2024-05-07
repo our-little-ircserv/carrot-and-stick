f71b58d4cd9c2d19a74c56ca3e0071793676956b
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-07 13:58:23 +0900

    feat: make Parse struct

--- irc_serv/Parser.hpp
+++ irc_serv/Parser.hpp
@@ -1,9 +1,19 @@
 #ifndef PARSER_HPP
 # define PARSER_HPP
 
+#include <string>
+#include <vector>
+
 namespace Parser
 {
-	//
+	struct MsgToken
+	{
+		std::string					prefix;
+		std::string					command;
+		std::vector< std::string >	params;
+	};
+
+	// fucntions...
 }
 
 #endif
