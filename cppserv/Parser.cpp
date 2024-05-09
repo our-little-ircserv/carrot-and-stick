f0e3c6cdb2ff66efe3cd9011d83118f1be87e58a
seojilee <seojilee@student.42seoul.kr>
2024-05-09 17:36:49 +0900

    style: remove temporary comments

--- cppserv/Parser.cpp
+++ cppserv/Parser.cpp
@@ -5,28 +5,28 @@
 #include <exception>
 #include "Parser.hpp"
 
-//extern std::string		password;
-//
-//uint16_t	Parser::checkArgValidity(int argc, char** argv) throw(Error)
-//{
-//	if (argc != 3)
-//	{
-//		throw Error(Error::EARGC, NULL);
-//	}
-//
-//	std::string	port(argv[1]);
-//	std::istringstream iss(port);
-//	int	port_checker;
-//	iss >> port_checker;
-//	if (iss.fail() || !iss.eof() \
-//			|| port_checker < 0 || port_checker > USHRT_MAX)
-//	{
-//		throw Error(Error::EAPORT, NULL);
-//	}
-//
-//	password = argv[2];
-//	return (uint16_t)port_checker;
-//}
+extern std::string		password;
+
+uint16_t	Parser::checkArgValidity(int argc, char** argv) throw(Error)
+{
+	if (argc != 3)
+	{
+		throw Error(Error::EARGC, NULL);
+	}
+
+	std::string	port(argv[1]);
+	std::istringstream iss(port);
+	int	port_checker;
+	iss >> port_checker;
+	if (iss.fail() || !iss.eof() \
+			|| port_checker < 0 || port_checker > USHRT_MAX)
+	{
+		throw Error(Error::EAPORT, NULL);
+	}
+
+	password = argv[2];
+	return (uint16_t)port_checker;
+}
 
 //Tokens:
 //-------
