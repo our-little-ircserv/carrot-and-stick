6b68276e63970fd2ee2786482afeaf303b3618a0
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-09 13:39:46 +0900

    feat: sync client model by seojilee version

--- irc_serv/Client.hpp
+++ irc_serv/Client.hpp
@@ -1,21 +1,28 @@
 #ifndef CLIENT_HPP
 # define CLIENT_HPP
 
+#include <netinet/ip.h>
+
 # include <string>
 # include <vector>
 
-class Client
+class	Client
 {
 	public:
-		//
-	
+		std::vector<std::string>	_read_buf;
+		std::vector<std::string>	_write_buf;
+
+		Client(int t_sockfd);
+		int	getSocketFd() const;
+		int	getWritable() const;
+		void	setWritable(bool t_writable);
+
 	private:
-		int sd;
-		std::string name;
-		std::string nickname;
-		std::string host;
-		std::string read_buf;
-		std::vector< std::string > write_buf;
+		int					_sockfd;
+		struct sockaddr_in	_host;
+		std::string			_nickname;
+		std::string			_username;
+		std::string			_realname;
 };
 
 #endif
