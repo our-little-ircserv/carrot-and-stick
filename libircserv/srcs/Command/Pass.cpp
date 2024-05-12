843642ba627be5d730fbb8011efb6c2063558693
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-12 23:46:15 +0900

    feat : change pass function's parameter again - remove const for edit server or client

--- libircserv/srcs/Command/Pass.cpp
+++ libircserv/srcs/Command/Pass.cpp
@@ -8,20 +8,16 @@
 // 	//
 // }
 
-void	Command::pass(const IRC& server, int sd, const std::vector< std::string > params) throw (Error)
+void	Command::pass(IRC& server, Client& client, const std::vector< std::string > params) throw (Error)
 {
-	Client* info;
-
-	info = &(server._clients[sd]);
-
 	// 이미 authorized 되었다면 무시한다
-	if (info->getRegisterLevel() > Client::NONE)
+	if (client.getRegisterLevel() > Client::NONE)
 		return ;
 
 	// 그렇지 않다면 authorization을 수행한다
 	if (server._password == params[0])
 	{
-		info->setRegisterLevel(Client::AUTHORIZED);
+		client.setRegisterLevel(Client::AUTHORIZED);
 	}
 	else
 	{
