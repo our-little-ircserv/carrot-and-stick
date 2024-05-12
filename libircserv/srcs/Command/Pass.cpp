6188ca28bbc3503e619d00926421426c4c50b837
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-12 15:16:16 +0900

    feat : use Client's getset function

--- libircserv/srcs/Command/Pass.cpp
+++ libircserv/srcs/Command/Pass.cpp
@@ -15,13 +15,13 @@ void	Command::pass(const IRC& server, int sd, const std::vector< std::string > p
 	info = &(server._clients[sd]);
 
 	// 이미 authorized 되었다면 무시한다
-	if (info->_register_level > 0)
+	if (info->getRegisterLevel() > Client::NONE)
 		return ;
 
 	// 그렇지 않다면 authorization을 수행한다
 	if (server._password == params[0])
 	{
-		info->_register_level = 1;
+		info->setRegisterLevel(Client::AUTHORIZED);
 	}
 	else
 	{
