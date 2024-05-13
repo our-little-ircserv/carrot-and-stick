ec74e9e0927e1f393c8d343342dfd8e342c92b66
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-13 18:08:52 +0900

    backup : go to dinner

--- libircserv/srcs/Command/Join.cpp
+++ libircserv/srcs/Command/Join.cpp
@@ -61,7 +61,40 @@ struct Command::Join	Parser::join(const std::vector< std::string >& params) thro
 
 void	Command::join(IRC& server, Client& client, const std::vector< std::string >& params) throw (Error)
 {
-	struct Command::Join data = ::Parser::join(params);
+	struct Command::Join data;
 
-	// ...
+	data = ::Parser::join(params);
+	/*
+
+	채널목록을 순회하며
+	1. 채널이 존재하는지 확인한다, 없다면 새로 생성한다. (이름이 여럿일때 유효하지 않은 이름이 섞여있는 경우 동작 확인할것)
+	2. 채널의 모드를 확인한다.(모드간 우선순위 확인할것)
+	3. 모드에 따른 확인 절차를 거치고 멤버를 추가한다.
+
+	isExistChannel
+	true -> getChannel -> reference? 초기화가 아니면 값 대입이 불가능 -> pointer*
+
+	*/
+	std::vector< std::string >::iterator it;
+	std::vector< std::string >::iterator ite;
+
+	it = data.channels.begin();
+	ite = data.channels.end();
+
+	while (it != ite)
+	{
+		Channel* channel = server.searchChannel(*it);
+		if (channel != NULL)
+		{
+			channel->addMember(client);
+		}
+		else
+		{
+			std::string mode = 0;
+			channel = server.createChannel(client, enum, *it, mode);
+			channel->addMember(client);
+		}
+
+		it++;
+	}	
 }
