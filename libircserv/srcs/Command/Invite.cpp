d9ed49cd0011c80b03120ebc91a7e0e0026759f8
seojilee <seojilee@student.42seoul.kr>
2024-05-13 14:38:28 +0900

    feat: add parsing func to command INVITE

--- libircserv/srcs/Command/Invite.cpp
+++ libircserv/srcs/Command/Invite.cpp
@@ -1,6 +1,18 @@
 #include "Parser.hpp"
 #include "Command.hpp"
 
-struct Command::Invite	Parser::invite(const std::vector< std::string >& params)
+struct Command::Invite	Parser::invite(const std::vector< std::string >& params) throw(Error)
 {
+	struct Command::Invite	data;
+
+	if (params.size() < 2)
+	{
+		std::string	concat_params = Parser::concat_string_vector(params);
+		throw Error(Error::ENEPARM, concat_params.c_str());
+	}
+
+	data.nickname = params[0];
+	data.channel = params[1];
+
+	return data;
 }
