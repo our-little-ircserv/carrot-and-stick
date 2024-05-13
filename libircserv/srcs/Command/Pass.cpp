00a771c5400c7df846da3e2a0e4b176c8fe6861d
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-13 13:13:47 +0900

    feat : call getter of IRC's password feat : change parameter - follow Command::cmdFunctions's parameter

--- libircserv/srcs/Command/Pass.cpp
+++ libircserv/srcs/Command/Pass.cpp
@@ -8,14 +8,14 @@
 // 	//
 // }
 
-void	Command::pass(IRC& server, Client& client, const std::vector< std::string > params) throw (Error)
+void	Command::pass(IRC& server, Client& client, const std::vector< std::string >& params) throw (Error)
 {
 	// 이미 authorized 되었다면 무시한다
 	if (client.getRegisterLevel() > Client::AUTHORIZED)
 		return ;
 
 	// 그렇지 않다면 authorization을 수행한다
-	if (server._password == params[0])
+	if (server.getPassword() == params[0])
 	{
 		client.setRegisterLevel(Client::AUTHORIZED);
 	}
