7f2f01e2660d72419bbce4a69297cc7bd4146d80
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-16 22:41:52 +0900

    feat : memo for idea

--- libircserv/srcs/Command/Invite.cpp
+++ libircserv/srcs/Command/Invite.cpp
@@ -16,3 +16,15 @@ struct Command::Invite	Parser::invite(const std::vector< std::string >& params) 
 
 	return data;
 }
+
+void	Command::invite(IRC& server, Client& client, const std::vector< std::string >& params) throw (Error)
+{
+	/*
+		초대하는 자가 해당 채널에 접속중인지 확인 - 해당 채널이 실존하는지는 검사하지 않음
+		초대받는 자가 실존하는 유저인지 검사
+		초대받는 자의 client객체를 구해야하니 getClient 메서드 사용 - nickname으로 검색
+		초대받는 자가 해당 채널에 이미 존재하는지 확인
+		invite_list에서 중복확인후 추가 -> 추후 join시에 invite_list에서 제거하는것 검토
+		채널 매개변수가 여러개여도 첫번째것만 적용
+	*/
+}
