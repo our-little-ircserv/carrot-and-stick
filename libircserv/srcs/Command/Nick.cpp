1e48ee818dfab42970269ef8b006ff124ff5785b
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-17 16:11:43 +0900

    feat : implement nick command

--- libircserv/srcs/Command/Nick.cpp
+++ libircserv/srcs/Command/Nick.cpp
@@ -39,5 +39,25 @@ std::string	Parser::nick(const std::vector< std::string >& params) throw(Error)
 
 void Command::nick(IRC& server, Client& client, const std::vector< std::string >& params) throw (Error)
 {
-	//
+	std::string new_nickname;
+
+	new_nickname = Parser::nick(params);
+	// 인자 수는 유효하나 닉네임의 내용이 비어있을때
+	// ERR_NONICKNAMEGIVEN
+	if (new_nickname.size() == 0)
+	{
+		//
+	}
+
+	// 닉네임 형식에 맞지 않을 때 (Parser::nick에서 유효성 검사)
+	// ERR_ERRONEUSNICKNAME
+
+	// 이미 사용중인 닉네임일 때
+	// ERR_NICKNAMEINUSE
+	if (!(server.searchClient(new_nickname) == NULL))
+	{
+		//
+	}
+
+	client.setNickname(new_nickname);
 }
