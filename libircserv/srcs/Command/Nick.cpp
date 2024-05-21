e0be290f2c47be30844c84a5ed75908e1e92535e
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-22 07:59:44 +0900

    feat : improve registration functions

--- libircserv/srcs/Command/Nick.cpp
+++ libircserv/srcs/Command/Nick.cpp
@@ -49,57 +49,48 @@ void Command::nick(IRC& server, Client& client, const struct Parser::Data& data)
 	std::string					new_nickname;
 	std::vector< std::string >	r_params;
 
-	new_nickname = Parser::nick(client, data.parameters);
-	// 인자 수는 유효하나 닉네임의 내용이 비어있을때
-	// ERR_NONICKNAMEGIVEN
-	if (new_nickname.size() == 0)
+	if (client.getRegisterLevel() == Client::NONE)
 	{
-		//
+		return ;
 	}
 
-	// 닉네임 형식에 맞지 않을 때 (Parser::nick에서 유효성 검사)
-	// ERR_ERRONEUSNICKNAME
-
+	new_nickname = Parser::nick(client, data.parameters);
 	// 이미 사용중인 닉네임일 때
-	// ERR_NICKNAMEINUSE
 	if (!(server.searchClient(new_nickname) == NULL))
 	{
-		//
+		r_params.push_back(client.getNickname());
+		r_params.push_back(new_nickname);
+		throw Reply(Reply::ERR_NICKNAMEINUSE, r_params);
 	}
 
 	client.setNickname(new_nickname);
+	if (client.getRegisterLevel() == Client::AUTHORIZED && (client.getUsername() == "") == false)
+	{
+		client.setRegisterLevel(Client::REGISTERED);
+		// 환영합니다
+		std::set< Client* > target_list;
+		target_list.insert(&client);
+		r_params.push_back(client.getNickname());
 
-	// broadcast on server
-	// std::set< Client* > target_list;
-	// {
-	// 	const std::vector< std::string > chan_list = client.getChannelList();
+		{
+			Reply rp(Reply::RPL_WELCOME, r_params);
+			server.deliverMsg(target_list, rp.getReplyMessage());
+		}
+		{
+			Reply rp(Reply::RPL_YOURHOST, r_params);
+			server.deliverMsg(target_list, rp.getReplyMessage());
+		}
+		{
+			Reply rp(Reply::RPL_CREATED, r_params);
+			server.deliverMsg(target_list, rp.getReplyMessage());
+		}
+		{
+			Reply rp(Reply::RPL_MYINFO, r_params);
+			server.deliverMsg(target_list, rp.getReplyMessage());
+		}
+		return ;
+	}
 
-	// 	for (size_t i = 0; i < chan_list.size(); i++)
-	// 	{
-	// 		Channel* channel = server.searchChannel(chan_list[i]);
-
-	// 		std::map<Client*, bool>::iterator it = channel->getMemberBegin();
-	// 		std::map<Client*, bool>::iterator ite = channel->getMemberEnd();
-
-	// 		for (; it != ite; it++)
-	// 		{
-	// 			target_list.insert(it->first);
-	// 		}
-	// 	}
-	// }
-	// std::set< Client* > target_list;
-	// std::set< Client* > t_list;
-	// {
-	// 	const std::vector< std::string > chan_list = client.getChannelList();
-
-	// 	for (size_t i = 0; i < chan_list.size(); i++)
-	// 	{
-	// 		Channel* channel = server.searchChannel(chan_list[i]);
-	// 		t_list = channel->getMemberSet();
-
-	// 		target_list.insert(t_list.begin(), t_list.end());
-	// 	}
-	// }
 	std::vector< std::string > target_names;
 	target_names.push_back(client.getNickname());
 	target_names.insert(target_names.end(), client.getChannelList().begin(), client.getChannelList().end());
--- libircserv/srcs/Command/User.cpp
+++ libircserv/srcs/Command/User.cpp
@@ -21,8 +21,45 @@ struct Command::User	Parser::user(const Client& client, const std::vector< std::
 
 void	Command::user(IRC& server, Client& client, const struct Parser::Data& data) throw (Reply)
 {
-	struct Command::User	p_data = Parser::user(client, data.parameters);
+	struct Command::User		p_data = Parser::user(client, data.parameters);
+	std::vector< std::string >	r_params;
+
+	if (client.getRegisterLevel() == Client::NONE)
+	{
+		return ;
+	}
+	else if (client.getRegisterLevel() == Client::REGISTERED)
+	{
+		r_params.push_back(client.getNickname());
+		throw Reply(Reply::ERR_ALREADYREGISTRED, r_params);
+	}
 
 	client.setUsername(p_data.username);
 	client.setRealname(p_data.real_name);
+	
+	if ((client.getNickname() == "*") == false)
+	{
+		client.setRegisterLevel(Client::REGISTERED);
+		// 환영합니다
+		std::set< Client* > target_list;
+		target_list.insert(&client);
+		r_params.push_back(client.getNickname());
+
+		{
+			Reply rp(Reply::RPL_WELCOME, r_params);
+			server.deliverMsg(target_list, rp.getReplyMessage());
+		}
+		{
+			Reply rp(Reply::RPL_YOURHOST, r_params);
+			server.deliverMsg(target_list, rp.getReplyMessage());
+		}
+		{
+			Reply rp(Reply::RPL_CREATED, r_params);
+			server.deliverMsg(target_list, rp.getReplyMessage());
+		}
+		{
+			Reply rp(Reply::RPL_MYINFO, r_params);
+			server.deliverMsg(target_list, rp.getReplyMessage());
+		}
+	}
 }
