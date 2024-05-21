edce4f84aae190e1f90c911a78ab7a15e08bcc19
YongHyun-Yi <dldydgus1995@naver.com>
2024-05-22 00:08:31 +0900

    add : part.cpp

---
+++ libircserv/srcs/Command/Part.cpp
@@ -0,0 +1,12 @@
+#include "Parser.hpp"
+#include "Command.hpp"
+
+struct Command::Part	Parser::part(const Client& client, const std::vector< std::string >& params) throw(Reply)
+{
+	//
+}
+
+void	Command::part(IRC& server, Client& client, const struct Parser::Data& data) throw (Reply)
+{
+	//
+}
